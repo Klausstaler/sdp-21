c57e421055f477d50426729d5ae5139853612f22
tgdivy <s1885517@ed.ac.uk>
2021-02-08 03:47:06 +0000

    changing packets

--- simulation/controllers/RFID_emit/RFID_emit.py
+++ simulation/controllers/RFID_emit/RFID_emit.py
@@ -12,12 +12,18 @@ emitter.setChannel(0)
 
 # print("what", emitter)
 # emitter.enable(TIME_STEP)
+
+# print("My position", robot.getPosition())
+
 i = 0
 while robot.step(32) != -1:
-    message = struct.pack("5s",b"abcde")
+    # print("emitted")
+
+    message = struct.pack("l",i)
     val = emitter.send(message)
-    print("emitted", val, i)
-    i+=1
+    i+=val
+    print("emitted 222", i, val)
+
     # print(emitter.getChannel())
     # pass
     
--- simulation/controllers/RFID_recieve/RFID_recieve.py
+++ simulation/controllers/RFID_recieve/RFID_recieve.py
@@ -2,7 +2,7 @@ from controller import Robot, Emitter
 import struct
 robot = Robot()
 
-TIME_STEP = 1
+TIME_STEP = 32
 
 receiver = robot.getDevice("receiver")
 receiver.setChannel(0)
@@ -17,6 +17,8 @@ while robot.step(TIME_STEP) != -1:
     if (queLen>0):
         message=receiver.getData()
         # print(message)
-        dataList=struct.unpack("5s",message)
-        val = dataList[0].decode("utf-8") 
-        print("message is", val)
+        dataList=struct.unpack("l",message)
+        # val = dataList[0].decode("utf-8")
+        val = dataList[0] 
+        print("message is", val, queLen)
+        receiver.nextPacket()
