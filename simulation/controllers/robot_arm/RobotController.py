a7f476c1fb4ba7a7245ce416be042ce05a598924
Klausstaler <fredrik.klauss@gmx.de>
2021-02-08 01:44:15 +0000

    Refactoring

--- simulation/controllers/robot_arm/RobotController.py
+++ simulation/controllers/robot_arm/RobotController.py
@@ -1,11 +1,27 @@
 from controller import Supervisor
+import numpy as np
 import sys, tempfile
+from typing import Union
 try:
     import ikpy
     from ikpy.chain import Chain
+    from ikpy.utils import geometry
 except ImportError:
     sys.exit('The "ikpy" Python module is not installed. '
              'To run this sample, please upgrade "pip" and install ikpy with this command: "pip install ikpy"')
+
+
+def convert_rpy(rpy: Union[list, np.array]) -> Union[list, np.array]:
+    """
+    pretty annoying: webots flips yaw and pitch vs standard convention, also the angle is reversed
+    :param rpy: rpy in webots format
+    :return: rpy in usual format
+    """
+    rpy[0], rpy[1], rpy[2] = -rpy[0], -rpy[1], -rpy[2]
+    rpy[1], rpy[2] = rpy[2], rpy[1]
+    return rpy
+
+
 
 class RobotController(Supervisor):
 
@@ -15,6 +31,9 @@ class RobotController(Supervisor):
         # Initialize the arm motors and encoders.
         super().__init__()
         self.arm_chain = self.get_armchain()
+        self.imu, self.suction_cup = self.getDevice("robot_imu"), self.getDevice("robot_box_connector")
+        self.imu.enable(timestep)
+        self.suction_cup.enablePresence(timestep)
         self.motors = []
         self.timestep = timestep
         for link in self.arm_chain.links:
@@ -24,15 +43,48 @@ class RobotController(Supervisor):
                 position_sensor = motor.getPositionSensor()
                 position_sensor.enable(self.timestep)
                 self.motors.append(motor)
-        print(self.arm_chain)
-        print(len(self.motors))
-        #print(self.getUrdf().encode('utf-8'))
-        #with open("kuka.urdf", "w") as f:
-        #    f.write(self.getUrdf())
-    def get_armchain(self):
+
+        # Deactivate fixed joints
+        for i in [0, 1]:
+            self.arm_chain.active_links_mask[i] = False
+
+    def get_armchain(self) -> Chain:
         filename = None
         with tempfile.NamedTemporaryFile(suffix='.urdf', delete=False) as file:
             filename = file.name
             file.write(self.getUrdf().encode('utf-8'))
         arm_chain = Chain.from_urdf_file(filename)
         return arm_chain
+
+    def move_endeffector(self, coordinate: np.array):
+        """
+        Moves end-effector to an x,y,z position relative to the base of the arm.
+        :param coordinate: The x,y,z coordinate relative to the base of the arm
+        :return:
+        """
+        # 0's for fixed joints, rest of the links have a joint associated with them
+        initial_joints = [0, 0] + [m.getPositionSensor().getValue() for m in self.motors]
+        ik_results = self.arm_chain.inverse_kinematics(coordinate, target_orientation=np.array([-1, 0, 1]),
+                                                                   orientation_mode="Z",
+                                                                   max_iter=4,
+                                                                   initial_position=initial_joints)
+
+        for i, motor in enumerate(self.motors):
+            motor.setPosition(ik_results[i+2]) # ignore first two joints as it does not have an associated motor
+
+    def convert_relative(self, global_coord: np.array) -> np.array:
+        """
+        Converts a global coordinate to a relative coordinate in the frame of the robot.
+        :param global_coord: The global coordiante frame
+        :return:
+        """
+        arm_pos = np.array(self.getSelf().getPosition())
+        relative_pos = global_coord - arm_pos
+        rot_mat = geometry.rpy_matrix(*self.get_rpy())
+        relative_pos = rot_mat @ relative_pos
+        return relative_pos
+
+    def get_rpy(self):
+        return convert_rpy(self.imu.getRollPitchYaw())
+
+
--- simulation/controllers/robot_arm/robot_arm.py
+++ simulation/controllers/robot_arm/robot_arm.py
@@ -9,66 +9,24 @@ robot_controller = RobotController(timestep=128)
 
 # Get the arm and target
 target = robot_controller.getFromDef("CONNECTOR_BOX1")
-arm = robot_controller.getSelf() # get base of robot arm
-
-for i in [0, 1]:
-    robot_controller.arm_chain.active_links_mask[i] = False
-# enable robot connector
-robot_connector = robot_controller.getDevice("robot_box_connector")
-robot_connector.enablePresence(128)
-
-imu = robot_controller.getDevice("robot_imu")
-imu.enable(128)
 
 was_released = False
-was_first = False
-print(arm.getPosition())
-final_target = [-.1, 0, 0.]
+
+final_target = np.array([-.1, 0, 0.])
 while robot_controller.step(robot_controller.timestep) != -1:
-    is_present = robot_connector.getPresence()
-    is_locked = robot_connector.isLocked()
+    is_present = robot_controller.suction_cup.getPresence()
+    is_locked = robot_controller.suction_cup.isLocked()
 
-    rot_mat = geometry.rpy_matrix(*imu.getRollPitchYaw())
-    print(rot_mat)
-    print(imu.getRollPitchYaw())
-    # Get the absolute postion of the target and the arm base.
-    arm_pos, target_pos = np.array(arm.getPosition()), np.array(target.getPosition())
-    #target_pos = np.array(target_pos)@rot_mat
-    # Compute the position of the target relatively to the arm.
 
-    # later we need to define a transform from the world frame to the robot frame
-    relative_pos = target_pos - arm_pos
-    relative_pos[0] += 0.01
-    relative_pos = relative_pos@rot_mat
-    x,y,z = relative_pos
-    print(x,y,z)
-    print("ARM POS", arm_pos)
-    print("TARGET POS", target_pos)
-    """
-    x = target_pos[0] - arm_pos[0] + 0.01 # small constant to not hit the box
-    y = target_pos[1] - arm_pos[1]
-    z = target_pos[2] - arm_pos[2]
-    """
-
+    # Get the absolute postion of the target
+    target_pos = np.array(target.getPosition())
+    relative_target = robot_controller.convert_relative(target_pos)
     if is_locked:
-        x, y, z = final_target
-    # First 0 for base link, rest of the links have a joint associated with them
-    initial_joints = [0, 0] + [m.getPositionSensor().getValue() for m in robot_controller.motors]
-    ik_results = robot_controller.arm_chain.inverse_kinematics([x,y,z], target_orientation=[-1,0,1],
-                                                               orientation_mode="Z",
-                                                               max_iter=IKPY_MAX_ITERATIONS, initial_position=initial_joints)
-    end_effector_pos = robot_controller.arm_chain.forward_kinematics(ik_results)[:3, 3] + arm_pos
-    #print(ik_results)
-    #print(target_pos)
-    print("NEXT TURN")
-    if is_present and not was_released and not is_locked:
-        #joint_config = initial_joints[:]
-        #joint_config[2] = 0.0
-        final_target[1], final_target[2] = y, z
+        relative_target = final_target
 
-        robot_connector.lock()
+    # lock the parcel, set the next target as the transport position for the parcel
+    if is_present and not was_released and not is_locked:
+        final_target[1], final_target[2] = relative_target[1:]
+        robot_controller.suction_cup.lock()
     if not was_released:
-        #print(ik_results)
-        for i, motor in enumerate(robot_controller.motors):
-            motor.setPosition(ik_results[i+2]) # ignore first joint as it does not have an associated motor
-""""""""
+        robot_controller.move_endeffector(relative_target)
