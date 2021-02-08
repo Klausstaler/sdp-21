fb73e3dd59d892ae68556896570716225a60ae00
tgdivy <s1885517@ed.ac.uk>
2021-02-08 02:01:56 +0000

    basic setup ready

--- simulation/controllers/RFID_recieve/RFID_recieve.py
+++ simulation/controllers/RFID_recieve/RFID_recieve.py
@@ -4,11 +4,11 @@ robot = Robot()
 
 TIME_STEP = 32
 
-reciever = robot.getDevice("reciever")
-
+reciever = robot.getDevice("receiver")
+reciever.enable(TIME_STEP)
 # print("what", emitter)
 # emitter.enable(TIME_STEP)
 
-while robot.step(32) != -1:
-    print("Not Hello World!")
-    print(emitter.getRange())
+while robot.step(TIME_STEP) != -1:
+    print("WHATT!")
+    print(reciever.getSamplingPeriod())
