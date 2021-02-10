3d812565d22fee31123d2b498310f8fdcd36b605
tgdivy <s1885517@ed.ac.uk>
2021-02-10 06:29:51 +0000

    vs code setup

--- simulation/controllers/RFID_emit/RFID_emit.py
+++ simulation/controllers/RFID_emit/RFID_emit.py
@@ -1,25 +1,13 @@
 from controller import Robot, Emitter
 import struct
 
-
-
-
-
-
-
-
-
-
-
 robot = Robot()
 
 TIME_STEP = 32
 
 emitter = robot.getDevice("emitter")
-
 # All channels
 emitter.setChannel(0)
-
 i = 0
 while robot.step(32) != -1:
     # print("emitted")
