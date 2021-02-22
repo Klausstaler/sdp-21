9b6c56dd47b9e5f8e3f016db7c0e4034a8ef2279
JIAQING XIE <55912913+JIAQING-XIE@users.noreply.github.com>
2021-02-22 20:53:51 +0800

    Add files via upload

--- simulation/controllers/line_follower/robot_control.py
+++ simulation/controllers/line_follower/robot_control.py
@@ -3,6 +3,12 @@ import numpy as np
 IKPY_MAX_ITERATIONS = 4
 TIMESTEP = 128
 
+encoder = {"straight": 1, "left": 2, "right": 3, "back": 4}
+
+direction_arr = [2, 2, 3, 2, 3, 3, 3, 3, 2, 3, 2, 2]
+
+
+
 # Initialize the Webots Supervisor.
 robot_controller = RobotController(timestep=TIMESTEP)
 
@@ -10,8 +16,9 @@ was_released = False
 
 x_pos = -.5 # go close to target in increments of .01
 while robot_controller.step(TIMESTEP) != -1:
-    robot_controller.strafeForward(8.0)
-    
+    #robot_controller.strafeForward(8.0)
+    robot_controller.line_tracking()
+    """
     is_present = robot_controller.arm.suction_cup.getPresence()
     is_locked = robot_controller.arm.suction_cup.isLocked()
     if is_present and not was_released and not is_locked:
@@ -22,4 +29,5 @@ while robot_controller.step(TIMESTEP) != -1:
         robot_controller.lift_motor.setPosition(.73)
         x_pos = robot_controller.arm.try_pickup(x_pos)
         print(x_pos)
+    """   
     
