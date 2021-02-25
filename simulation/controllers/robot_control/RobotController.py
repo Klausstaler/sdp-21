d6f1543c6f4203fd974d54c7e28af582c84141fa
tgdivy <s1885517@ed.ac.uk>
2021-02-25 22:55:32 +0000

    A lot more precise movements

--- simulation/controllers/robot_control/RobotController.py
+++ simulation/controllers/robot_control/RobotController.py
@@ -5,6 +5,9 @@ from types import SimpleNamespace
 
 class Navigation:
     def __init__(self, robot: Robot, timestep=128):
+        self.max_val = 150
+        self.low_val = 120
+        self.high_val = 142
         #IR sensors
         IR = {}
         for name in ["ds_left", "ds_right", "ds_mid"]:
@@ -21,15 +24,43 @@ class Navigation:
             Wheels[name[6:]].setVelocity(0.0)
         self.wheels = SimpleNamespace(**Wheels)
 
+    def sensor_value(self, name, value=None):
+        if value is None:
+            if name=="left":
+                value = self.IR.left.getValue()
+            elif name=="right":
+                value = self.IR.left.getValue()
+            elif name == "mid":
+                value = self.IR.left.getValue()
+        if value<=self.low_val:
+            return 0
+        elif value<= self.high_val:
+            return 0.5
+        else:
+            return 1
+
+    def sensors_values(self, left, mid, right):
+        values = (left, mid, right)
+        ans = True
+        for i, name in enumerate(["left","mid","right"]):
+            ans = ans and (self.sensor_value(name) in values[i])
+        return ans
+
+    def set_wheel_speeds(self, FL, FR, BL, BR):
+        self.wheels.FL.setVelocity(FL)
+        self.wheels.FR.setVelocity(FR)
+        self.wheels.BL.setVelocity(BL)
+        self.wheels.BR.setVelocity(BR)
+
     # n=1 is turn until the first line you see, 
-    def turn_until_line_n(self, n=1, new=False, speed = 4):
+    def turn_until_line_n(self, n=1, new=False, speed = 6):
         if new:
             self.n_lines = n
             self.n_line_token = False
         # print(self.n_lines, self.n_line_token)
 
         if self.n_lines>1:
-            self.turn_clockwise(speed)
+            self.turn(speed, clock=True)
             if self.line_detected() and not self.n_line_token:
                 self.n_line_token = True
             elif self.n_line_token == True and not self.line_detected():
@@ -37,7 +68,7 @@ class Navigation:
                 self.n_line_token = False
             return False
         elif self.n_lines==1:
-            self.turn_clockwise(speed)
+            self.turn(speed, clock=True)
             if self.line_detected():
                 self.stop()
                 return True
@@ -45,59 +76,53 @@ class Navigation:
             self.stop()
             return True
 
-    def follow_line(self, speed = 4):
-        leftSpeed = speed
-        rightSpeed = speed
-        turnfactor = 10
+    def follow_line(self, speed = 3):
         nearLine = True
-        # if True:
-        # print(self.IR.left.getValue(),self.IR.mid.getValue(),self.IR.right.getValue())
 
-        if self.IR.left.getValue() <= 900 and self.IR.right.getValue()<=900 and self.IR.mid.getValue()>900:
-            leftSpeed = speed*2
-            rightSpeed = speed*2
-        elif self.IR.left.getValue() >= 900 and self.IR.right.getValue()<=900:
-            leftSpeed = speed
-            rightSpeed = speed/turnfactor
-        elif self.IR.left.getValue() <= 900 and self.IR.right.getValue()>=900:
-            leftSpeed = speed/turnfactor
-            rightSpeed = speed
-        # elif self.IR.left.getValue() >= 990 and self.IR.right.getValue()>=990 and self.IR.mid.getValue()>990:
-        #     # print("Junction")
-        #     leftSpeed = 0
-        #     rightSpeed = 0
-        #     nearLine = False
-        else:
-            # print("abababa")
+        if self.sensors_values(left=[0], mid=[1], right=[0]):
+            print("case straight")
+            self.move_forward(speed*2)
+        elif self.sensors_values(left=[0], mid=[0,0.5,1], right=[1]):
+            print("case right")
+            self.turn_on_wheel_axis(speed, left=False, top=True)
+        elif self.sensors_values(left=[1], mid=[0,0.5,1], right=[0]):
+            print("case left")
+            self.turn_on_wheel_axis(speed, left=True, top=True)
+        elif self.sensors_values(left=[0], mid=[0], right=[0]):
             nearLine = False
+            self.move_forward(speed/2)
 
-        self.wheels.FL.setVelocity(leftSpeed)
-        self.wheels.FR.setVelocity(rightSpeed)
-        self.wheels.BL.setVelocity(rightSpeed)
-        self.wheels.BR.setVelocity(leftSpeed)
-        
         return nearLine
 
-    def turn_clockwise(self, speed):
-        self.wheels.FL.setVelocity(-speed)
-        self.wheels.FR.setVelocity(speed)
-        self.wheels.BL.setVelocity(-speed)
-        self.wheels.BR.setVelocity(speed)
-        print(self.line_detected())
-    
-    def stop(self):
-        self.wheels.FL.setVelocity(0)
-        self.wheels.FR.setVelocity(0)
-        self.wheels.BL.setVelocity(0)
-        self.wheels.BR.setVelocity(0)
-        # print(self.line_detected())
-    
+    def move_forward(self, speed):
+        self.set_wheel_speeds(speed, speed, speed, speed)
 
+    def stop(self):
+        self.set_wheel_speeds(0, 0, 0, 0)
+
+    def strafe(self, speed, right=True):
+        if not right:
+            speed *=-1
+        self.set_wheel_speeds(speed, -speed, -speed, speed)
+
+    def turn(self, speed, clock=True):
+        if not clock:
+            speed *=-1
+        self.set_wheel_speeds(speed, -speed, speed, -speed)
+    
+    def turn_on_wheel_axis(self, speed, left=True, top=True):
+        if top:
+            speed*=-1
+        if left:
+            self.set_wheel_speeds(0.1, speed, 0.1, speed)
+        else:
+            self.set_wheel_speeds(speed, 0.1, speed, 0.1)
+    
     def line_detected(self):
-        print(self.IR.left.getValue(),self.IR.mid.getValue(),self.IR.right.getValue())
-        if self.IR.left.getValue() <= 900 and self.IR.right.getValue()<=900 and self.IR.mid.getValue()>900:
+        print(self.IR.left.getValue(), self.IR.mid.getValue(),self.IR.right.getValue())
+        if self.sensors_values(left=[0], mid=[1, 0.5], right=[0]):
             return True
-        elif self.IR.left.getValue() >= 990 or self.IR.right.getValue()>=990 or self.IR.mid.getValue()>=990:
+        elif self.sensor_value("left")==1 or self.sensor_value("right")==1:
             return True
         else:
             return False
@@ -119,8 +144,9 @@ class RobotController(Robot):
 
     def reach_node(self, node):
         self.node_to_reach = node
-        self.nav.turn_clockwise(20)
-        return False
+        # self.nav.turn_clockwise(4)
+        # self.follow_line = self.nav.follow_line()
+        # return False
         if not self.reached_node:
             if self.follow_line:
                 # print("following line")
