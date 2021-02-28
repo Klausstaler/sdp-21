fdde0fc54b3eb659b4c3a633cfa35a0d75a619aa
tgdivy <s1885517@ed.ac.uk>
2021-02-28 14:09:59 +0000

    basic task setup

--- simulation/controllers/robot_control/robot_control.py
+++ simulation/controllers/robot_control/robot_control.py
@@ -12,16 +12,10 @@ net_interface = NetworkInterface()
 robot_controller = RobotController(timestep=TIMESTEP)
 
 
+
 while robot_controller.step(TIMESTEP) != -1:
     success, curr_task = False, net_interface.get_current_task()
-    if curr_task.task_type == TaskType.REACH_NODE:
-        success = robot_controller.reach_node(curr_task.params["node"])
-    if curr_task.task_type == TaskType.TURN_UNTIL:
-        success = robot_controller.turn_until(int(curr_task.params["n"]))
-    elif curr_task.task_type == TaskType.PICKUP_PARCEL:
-        success = robot_controller.arm.try_pickup()
-    elif curr_task.task_type == TaskType.RAISE_PLATFORM:
-        success = robot_controller.raise_platform(**curr_task.params)
+    
     if success:
         print("Finished", curr_task.task_type)
         net_interface.send_response(curr_task.task_type.value)
