f8a810c38336a090a7d65c0e43a8f7a6c1b024d5
Rohan Nittur <rohan.nittur@gmail.com>
2021-03-13 22:45:28 +0100

    Scissor Tests

--- simulation/controllers/test_scissor/test_scissor.py
+++ simulation/controllers/test_scissor/test_scissor.py
@@ -4,13 +4,39 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 from math import asin, sqrt, pi
+from types import SimpleNamespace
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+Wheels = {}
+for name in ['wheel_FL', 'wheel_FR', 'wheel_BL', 'wheel_BR']:
+    Wheels[name[6:]] = robot.getDevice(name)
+    # self.Wheels[name[6:]].enable(timestep)
+    Wheels[name[6:]].setPosition(float('inf'))
+    Wheels[name[6:]].setVelocity(0.0)
+wheels = SimpleNamespace(**Wheels)
 
+
+def set_wheel_speeds(FL, FR, BL, BR):
+    wheels.FL.setVelocity(FL)
+    wheels.FR.setVelocity(FR)
+    wheels.BL.setVelocity(BL)
+    wheels.BR.setVelocity(BR)
+    
+def set_pos(FL, FR, BL, BR):
+    wheels.FL.setPosition(FL)
+    wheels.FR.setPosition(FR)
+    wheels.BL.setPosition(BL)
+    wheels.BR.setPosition(BR)
+    
+    
+def move_forward(speed, distance, right=None, top=None, clock=None):
+    set_wheel_speeds(speed, speed, speed, speed)
+    set_pos(distance,distance,distance,distance)
+    
 
 def movePlatform(H,L,n):
     motors = [robot.getDevice(f"motor {i+1} {j}") for i in range(n) for j in ["left","right"]]
@@ -45,7 +71,9 @@ def movePlatform(H,L,n):
 
 
 
-movePlatform(1.5,0.6,4)  
+movePlatform(0.5,0.6,4)
+robot.step(2000)
+move_forward(5.0,30)  
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
