256c5bff23aa12f936aa555e1207798412296489
Klausstaler <fredrik.klauss@gmx.de>
2021-03-22 16:29:58 +0000

    Push new code

--- server/pickup_test.py
+++ server/pickup_test.py
@@ -11,20 +11,20 @@ from server.sample_db_output import db_output
 
 async def main():
     sched = Scheduler(Graph(db_output))
-    my_shelf = Shelf(1, 2, 16)
+    my_shelf = Shelf(1, 2, 18)
     shelf_info = ShelfInfo(my_shelf, 1)
-    parcel = Parcel(12., Size(.35, .35, .35), 16, shelf_info)
+    parcel = Parcel(12., Size(.35, .35, .35), 18, shelf_info)
     robot_size = Size(height=.25, length=.75, width=.7)
-    #sched.add_free_robot(Robot("2", robot_size, 56))
+    sched.add_free_robot(Robot("2", robot_size, 56))
     sched.add_free_robot(Robot("1", robot_size, 27))
     interface = NetworkInterface()
     server = CentralServer(sched, interface)
     task1 = asyncio.create_task(server.move_parcel(parcel, None))
     # await asyncio.gather(task1)
 
-    my_shelf = Shelf(1, 2, 38)
+    my_shelf = Shelf(1, 2, 41)
     shelf_info = ShelfInfo(my_shelf, 1)
-    parcel = Parcel(12., Size(.35, .35, .35), 38, shelf_info)
+    parcel = Parcel(12., Size(.35, .35, .35), 41, shelf_info)
     task2 = asyncio.create_task(server.move_parcel(parcel, None))
 
     await asyncio.gather(task1)
